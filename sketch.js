447c1485c910a30893c44124a16138ffbb658f8c
brainstormerjr <60263058+brainstormerjr@users.noreply.github.com>
2020-01-26 00:29:04 +0800

    Added ability to view different colors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 let rawData;
 let data;
-let rez = 10;
+let rez;
+let rezSlider;
 let countP;
+let currentColor;
+let labelRadio;
+
+let colorList = [
+  'Red',
+  'Blue',
+  'Green',
+  'Yellow',
+  'Pink',
+  'Purple',
+  'Orange',
+  'Brown'
+];
 
 //setup and initialize firebase
 const firebaseConfig = {
@@ -18,17 +32,50 @@ let database = firebase.database();
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowHeight, windowHeight);
   countP = createP('Database size: ');
+  countP.style('padding', '20px');
+  rezSlider = createSlider(1, width / 2, 10, 0.1);
+  labelRadio = createRadio();
+  for (let color of colorList) {
+    labelRadio.option(color);
+  }
+  labelRadio.option('All');
 }
 
 function gotData(data) {
   //print(data.val());
   rawData = data.val();
-  print('got data!');
+  //print('got data!');
+}
+
+function mousePressed() {
+  if (mouseX < width && mouseY < height) {
+    let x = floor(mouseX / rez);
+    let y = floor(mouseY / rez);
+    let index = x + y * floor(width / rez);
+    let shownData = [];
+    let shownKeys = [];
+    if (currentColor == 'All') {
+      for (let key in rawData) {
+        shownData.push(rawData[key]);
+      }
+    } else {
+      for (let key in rawData) {
+        if (rawData[key].label == currentColor) {
+          shownData.push(rawData[key]);
+          shownKeys.push(key);
+        }
+      }
+    }
+    print(`Key: ${shownKeys[index]}`);
+    print(shownData[index]);
+  }
 }
 
 function draw() {
+  rez = rezSlider.value();
+  currentColor = labelRadio.value();
   database.ref('colors').once('value', gotData);
   background(51);
   if (rawData) {
@@ -37,22 +84,40 @@ function draw() {
 
     let count = 0;
     for (let key in rawData) {
-      let r = rawData[key].r;
-      let g = rawData[key].g;
-      let b = rawData[key].b;
-      let c = color(r, g, b);
-      fill(c);
-      noStroke();
-      rect(x, y, rez, rez);
+      if (currentColor == 'All') {
+        let r = rawData[key].r;
+        let g = rawData[key].g;
+        let b = rawData[key].b;
+        let c = color(r, g, b);
+        fill(c);
+        noStroke();
+        rect(x, y, rez, rez);
 
-      x += rez;
+        x += rez;
 
-      if (x >= width) {
-        x = 0;
-        y += rez;
+        if (x > width) {
+          x = 0;
+          y += rez;
+        }
+        count++;
+      } else if (rawData[key].label == currentColor) { // && rawData[key].uid != 'JG5NR5dcVSSnVNvrZU9clFuzHel1'
+        let r = rawData[key].r;
+        let g = rawData[key].g;
+        let b = rawData[key].b;
+        let c = color(r, g, b);
+        fill(c);
+        noStroke();
+        rect(x, y, rez, rez);
+
+        x += rez;
+
+        if (x > width) {
+          x = 0;
+          y += rez;
+        }
+        count++;
       }
-      count++;
     }
-    countP.html(`Database size: ${count}`);
+    countP.html(`Database size : ${count}`);
   }
 }
